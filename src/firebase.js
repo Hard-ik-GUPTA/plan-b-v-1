28781ce46a42b026d1d622f4b800235ae0c4db27
Hardik Gupta <hardik09g@gmail.com>
2022-03-04 22:29:42 +0530

    version 1 working

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,14 @@ import { initializeApp } from 'firebase/app';
 import {getFirestore} from "@firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyADl8NiEhsu6lH9G7oL1xiPN56F5ke_S-8",
-    authDomain: "mathsquiz-848ba.firebaseapp.com",
-    databaseURL: "https://mathsquiz-848ba-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "mathsquiz-848ba",
-    storageBucket: "mathsquiz-848ba.appspot.com",
-    messagingSenderId: "967286619357",
-    appId: "1:967286619357:web:4c2b5b8a6c4f7668f2af34"
+    apiKey: "AIzaSyDxvcnqTni6kkM2SNQEsXyVI3zon_tALqs",
+    authDomain: "plan-b-416e4.firebaseapp.com",
+    projectId: "plan-b-416e4",
+    storageBucket: "plan-b-416e4.appspot.com",
+    messagingSenderId: "723325654397",
+    appId: "1:723325654397:web:303f332c4675982deea814",
+    measurementId: "G-LSXNN46NH3"
+  
   };
 
   const app= initializeApp(firebaseConfig)
